7a132810e84a724006aeeeede25f4ab08bf9fe38
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-09-13 17:20:53 +0800

    scroll using mouse move

--- app/components/MouseDemo.tsx
+++ app/components/MouseDemo.tsx
@@ -4,68 +4,138 @@ import React, { useRef, useState } from "react";
 
 gsap.registerPlugin(useGSAP);
 
-type Coordinates = { x: number; y: number; distX: number };
-type QuickSetterFn = (value: number | string) => void;
+type QuickSetterFn = (value: string | number) => void;
 
 export default function MouseDemo() {
   //useRef
   const container = useRef<HTMLDivElement | null>(null);
-
-  //state
-  const [coordinates, setCoordinates] = useState<Coordinates>({
-    x: 0,
-    y: 0,
-    distX: 0,
-  });
+  const setContainerOverFlow = useRef<QuickSetterFn | null>(null);
+  const setScrollX = useRef<QuickSetterFn | null>(null);
+  const scrollTween = useRef<gsap.core.Tween | null>(null);
 
   //useGsap - contextSafe
-  const { contextSafe } = useGSAP(() => {}, { scope: container });
+  const { contextSafe } = useGSAP(
+    () => {
+      //boxWrapper rect
+      const boxWrapper = container.current?.querySelector(
+        ".box-wrapper",
+      ) as HTMLElement;
+      const boxWrapperRect = boxWrapper.getBoundingClientRect();
+      const boxWrapperWidth = boxWrapperRect.width;
 
-  function handleMouseMove(e: React.MouseEvent<HTMLDivElement>): void {
-    const context = contextSafe(() => {
-      const rect = e.currentTarget.getBoundingClientRect();
-      const xAxis = e.nativeEvent.clientX - rect.left;
-      const yAxis = e.nativeEvent.clientY - rect.top;
+      //container rect
+      const containerRect = container.current?.getBoundingClientRect();
+      if (!containerRect) return;
+      const containerWidth = containerRect.width;
+      const containerCenter = containerWidth / 2;
+      console.log("containerRect.left", containerRect.left);
 
-      //calculates the distance between the width and the center of the div
-      const center = rect.width / 2;
-      const distanceX = Math.abs(center - xAxis);
+      //scroll computation
+      const scrollAmount = boxWrapperWidth - containerWidth;
 
-      const updatedCoordinates: Coordinates = {
-        x: xAxis,
-        y: yAxis,
-        distX: distanceX,
+      const updateSquash = () => {
+        //calculate the distance of the boxtiles to the container
+        const boxTiles =
+          container.current?.querySelectorAll("[class*=box-tile-]");
+        //convert node to array
+        const boxTilesArr = Array.from(boxTiles ?? []);
+
+        //array of quickSetter
+        const setScaleXList = boxTilesArr.map((tile) =>
+          gsap.quickSetter(tile, "scaleX"),
+        );
+
+        boxTilesArr.forEach((tile, index) => {
+          const tileRect = tile.getBoundingClientRect();
+          const distanceTile = tileRect.left - containerRect?.left;
+
+          const rawScale = gsap.utils.mapRange(120, 0, 1, 0.1, distanceTile);
+          const scaleX = gsap.utils.clamp(0.1, 1, rawScale);
+
+          setScaleXList[index](scaleX);
+        });
       };
-      setCoordinates(updatedCoordinates);
 
-      //quickSetter
-      const setScaleX = gsap.quickSetter(".box", "scaleX");
-      const setScaleY = gsap.quickSetter(".box", "scaleY");
+      //unmount the ticker at the bottom of useGSAP
+      gsap.ticker.add(updateSquash);
 
-      const scaleX = gsap.utils.mapRange(0, center, 2, 0.0, distanceX);
-      // const scaleY = gsap.utils.mapRange(0, 500, 1, 1.5, yAxis);
+      //tween
+      //it is default to (negative)scrollAmount to make the animation go left to right
+      //the animation scroll switch to right when the cursor is on the right side of the container.current
+      // left focus cursor gives negative value (cursor distance to the center of container.ref)
+      // right focus gives positive value (cusor distance to the center of container.ref)
+      // negative value of distance gives negavite timeScale -> this is going to rewind the animation of tween.X
+      // positive value of distance gives positive timeScale -> this is going to play forward the animation of tween.X
+      // bigger negative value / positive value will show a slower and faster animation
+      // mouse distance trigger is handled by handleMouseMove function
+      setContainerOverFlow.current = gsap.quickSetter(
+        container.current,
+        "overflow",
+      ) as QuickSetterFn;
+      setScrollX.current = gsap.quickSetter(boxWrapper, "x") as QuickSetterFn;
 
-      setScaleX(scaleX);
-      setScaleY(scaleX);
-    });
+      setContainerOverFlow.current("hidden");
+      setScrollX.current(containerCenter);
 
-    context();
-  }
+      //tween
+      scrollTween.current = gsap.to(boxWrapper, {
+        x: -scrollAmount,
+        duration: 10,
+        ease: "none",
+      });
+
+      const handleMouseMove = (e: MouseEvent, center: number): void => {
+        //distance of center to left and right side of the container.current
+        const distanceX = e.offsetX - containerCenter;
+        console.log("distance", distanceX);
+        const setTimeScale = gsap.utils.mapRange(
+          -center,
+          center,
+          -3,
+          3,
+          distanceX,
+        );
+
+        if (!scrollTween.current) return;
+        scrollTween.current.timeScale(setTimeScale);
+      };
+
+      //injects the listener to the container.current
+      container.current?.addEventListener("mousemove", (e: MouseEvent) => {
+        handleMouseMove(e, containerCenter);
+      });
+
+      return () => {
+        gsap.ticker.remove(updateSquash);
+      };
+    },
+    { scope: container },
+  );
 
   return (
-    <div className="flex flex-row justify-center items-center gap-3.5 ">
+    <div className="flex flex-row justify-center items-start">
       <div
         ref={container}
-        onMouseMove={handleMouseMove}
-        className="border-5 border-black h-[500px] w-[500px] flex flex-row justify-center items-center relative"
+        className="border-5 border-black h-[500px] w-[500px] flex flex-row items-center"
       >
-        {/* <div className="border h-full w-0.1"></div> */}
-        <div className="box border-2 rounded-2xl bg-amber-200 w-50 h-50 "></div>
+        <div className="box-wrapper flex flex-row gap-3">
+          {Array.from({ length: 10 }, (_, index) => {
+            return (
+              <div
+                key={index}
+                className={`box-tile-${index + 1} bg-blue-300 rounded-2xl flex flex-col justify-center items-center w-30 h-30 `}
+              >
+                {index + 1}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="bg-amber-300 w-30">
-        <p>mouse-X: {coordinates.x}</p>
+      <div className="bg-amber-300 w-50">
+        {/* <p>mouse-X: {coordinates.x}</p>
         <p>mouse-Y: {coordinates.y}</p>
         <p>distance-X: {coordinates.distX}</p>
+        <p>rotation: {coordinates.rotation}</p> */}
       </div>
     </div>
   );
