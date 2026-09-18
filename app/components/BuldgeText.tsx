b4332a80de22aa42fdd56f15ad9ed35f7b4bd7e8
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-09-18 14:31:11 +0800

    Updated animation of BudgeText component

--- app/components/BuldgeText.tsx
+++ app/components/BuldgeText.tsx
@@ -5,6 +5,71 @@ import { SplitText } from "gsap/SplitText";
 import React, { useRef } from "react";
 
 gsap.registerPlugin(useGSAP, SplitText, GSDevTools);
+
+gsap.registerEffect({
+  name: "buldgeShine",
+  extendTimeline: true,
+  defaults: {
+    color: "#1F3D1F",
+    shineColor: "#39AD39",
+    transformPerspective: 180,
+    stagger: 0.25,
+    rotationY: 35,
+    duration: 1,
+    z: 90,
+  },
+  effect: (
+    targets: gsap.DOMTarget,
+    config: {
+      color: string;
+      shineColor: string;
+      transformPerspective: number;
+      stagger: number;
+      rotationY: number;
+      duration: number;
+      z: number;
+    },
+  ) => {
+    const setColor = gsap.quickSetter(targets, "color");
+    setColor(config.color);
+    const setTransformPerspective = gsap.quickSetter(
+      targets,
+      "transformPerspective",
+      "px",
+    );
+    setTransformPerspective(config.transformPerspective);
+
+    const tl = gsap
+      .timeline()
+      .to(targets, {
+        z: config.z,
+        color: config.shineColor,
+        duration: config.duration,
+        stagger: { each: config.stagger, repeat: 1, yoyo: true },
+      })
+      .to(
+        targets,
+        {
+          duration: config.duration / 2,
+          rotateY: config.rotationY,
+          stagger: { each: config.stagger, repeat: 1, yoyo: true },
+        },
+        "<",
+      )
+      .to(
+        targets,
+        {
+          duration: config.duration / 2,
+          rotateY: -config.rotationY,
+
+          stagger: { each: config.stagger, repeat: 1, yoyo: true },
+        },
+        config.duration,
+      );
+
+    return tl;
+  },
+});
 export default function BuldgeText() {
   const container = useRef<HTMLDivElement>(null);
 
@@ -14,38 +79,10 @@ export default function BuldgeText() {
 
       const setOpacity = gsap.quickSetter(container.current, "opacity");
       setOpacity(1);
-      const setColor = gsap.quickSetter(split.chars, "color");
-      setColor("#36BBA7");
-      const setTransformPerspective = gsap.quickSetter(
-        split.chars,
-        "perspective",
-      );
-      setTransformPerspective("180");
 
       const tl = gsap
         .timeline()
-        .to(split.chars, {
-          duration: 0.3,
-          scaleX: 1.3,
-          stagger: 0.05,
-          z: 80,
-          rotateY: 35,
-          ease: "none",
-          color: "#C8EFE9",
-        })
-        .to(
-          split.chars,
-          {
-            duration: 0.3,
-            stagger: 0.05,
-            scaleX: 0,
-            z: 0,
-            rotateY: 0,
-            ease: "none",
-            color: "#36BBA7",
-          },
-          "0.3",
-        );
+        .buldgeShine(split.chars, { duration: 0.5, stagger: 0.1 });
 
       GSDevTools.create({ animation: tl });
     },
