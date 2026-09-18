a480e3c0e5b24b2613b451417557bee0d9f11a97
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-09-18 16:43:48 +0800

    For deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/gsap-sketchpad-v2",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
